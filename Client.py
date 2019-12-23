cef8a74ebd4e970be32c179ee94ffe8d30618aa1
SeniorHunter <shekari.ah99@gmail.com>
2019-12-23 15:11:13 +0330

    encapsulate some code

--- Client.py
+++ Client.py
@@ -1,5 +1,5 @@
 import logging
-import socket
+import socket as Socket
 import time
 import Packet
 from timer import Timer
@@ -11,12 +11,69 @@ SEND_FILE_PATH = b'send.jpg'
 TIMEOUT = 0.1
 PACKET_SIZE = 700  # dont change this shit pls
 WINDOW_SIZE = 10
+
 number_of_packets = 0
 packets = []
 last_ack = -1
 next_packet = 0
 timer = Timer(TIMEOUT)
 logger: logging
+packet: Packet
+
+
+def client(connection):
+    global packet
+    packet = Packet(connection)
+    initialize_logger()
+
+    global last_ack
+    global next_packet
+
+    to_buffer()
+
+    while True:
+        while not is_send_window_complete():
+            logging.info("send packet " + str(next_packet))
+            send_window()
+
+        if is_finished():
+            break
+
+        if not timer.is_running():
+            timer.start()
+            logging.info("timer start")
+
+        while timer.is_running() and not timer.time_out():
+            receive_ack()
+
+        if timer.time_out():
+            logging.info("time out")
+            timer.stop()
+            next_packet = last_ack
+
+        if is_finished():
+            packet.send_packet(0, True)
+            break
+
+
+def send_window():
+    global next_packet
+    packet.send_packet(next_packet, False, packets[next_packet])
+    next_packet += 1
+    time.sleep(0.0001)
+
+
+def receive_ack():
+    global last_ack
+    try:
+        ack, _, _ = packet.receive_packet()
+        if ack is not None:
+            logging.info("ack " + str(ack))
+            if ack >= last_ack:
+                last_ack = ack
+                timer.stop()
+    except:
+        pass
 
 
 def initialize_logger():
@@ -32,50 +89,6 @@ def is_finished():
 
 def is_send_window_complete():
     return not (next_packet - last_ack <= WINDOW_SIZE and next_packet < number_of_packets)
-
-
-def client(local_socket):
-    packet = Packet(local_socket)
-    initialize_logger()
-
-    global last_ack
-    global next_packet
-
-    to_buffer()
-
-    while True:
-        while not is_send_window_complete():
-            packet.send_packet(next_packet, False, packets[next_packet])
-            logging.info("send packet " + str(next_packet))
-            next_packet += 1
-            time.sleep(0.0001)
-
-        if is_finished():
-            break
-
-        if not timer.is_running():
-            timer.start()
-            logging.info("timer start")
-
-        while timer.is_running() and not timer.time_out():
-            try:
-                ack, is_EOT, _ = packet.receive_packet()
-                if ack is not None:
-                    logging.info("ack " + str(ack))
-                    if ack >= last_ack:
-                        last_ack = ack
-                        timer.stop()
-            except:
-                pass
-
-        if timer.time_out():
-            logging.info("time out")
-            timer.stop()
-            next_packet = last_ack
-
-        if is_finished():
-            packet.send_packet(0, True)
-            break
 
 
 def to_buffer():
@@ -96,7 +109,7 @@ def to_buffer():
         return
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.settimeout(False)
-    client(sock)
+with Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM) as socket:
+    socket.connect((HOST, PORT))
+    socket.settimeout(False)
+    client(socket)
--- Server.py
+++ Server.py
@@ -6,7 +6,45 @@ from Packet import Packet
 PORT = 65432
 HOST = '127.0.0.1'
 RECEIVED_FILE_PATH = b'receive.jpg'
+
 logger: logging
+sequence_number: int
+expected_number = 0
+packet: Packet
+
+
+def server(connection):
+    global packet
+    global sequence_number
+    packet = Packet(connection)
+    initialize_logger()
+    file = open_file()
+
+    while True:
+        sequence_number, is_EOT, data = packet.receive_packet()
+        if is_EOT:
+            logging.info("EOT received, finishing connection...")
+            packet.send_packet(-1, True)
+            break
+        logger.info("data number " + str(sequence_number) + " received")
+
+        if expected_number == sequence_number:
+            expected_ack(file, data)
+            logger.info("Ack send: " + str(sequence_number))
+        else:
+            # drop and send Ack(exp-num - 1)
+            packet.send_packet((expected_number - 1), False)
+            logger.info("(resubmit) Ack send: " + str(sequence_number))
+    logging.info("saving file...")
+    file.close()
+
+
+def expected_ack(file, data):
+    # send ack and write to file
+    global expected_number
+    packet.send_packet(sequence_number, False)
+    expected_number += 1
+    file.write(data)
 
 
 def initialize_logger():
@@ -24,40 +62,8 @@ def open_file():
         return
 
 
-def server(connection):
-    packet = Packet(connection)
-    initialize_logger()
-    file = open_file()
-
-    expected_number = 0
-    while True:
-        sequence_number, is_EOT, data = packet.receive_packet()
-        if is_EOT:
-            logging.info("EOT received, finishing connection...")
-            packet.send_packet(-1, True)
-            break
-        logger.info("data number " + str(sequence_number) + " received")
-
-        if expected_number == sequence_number:
-            # send ack and write to file
-            packet.send_packet(sequence_number, False)
-            logger.info("Ack send: " + str(sequence_number))
-            expected_number += 1
-            file.write(data)
-        else:
-            # drop and send Ack(exp-num - 1)
-            packet.send_packet((expected_number - 1), False)
-            logger.info("(resubmit) Ack send: " + str(sequence_number))
-    logging.info("saving file...")
-    file.close()
-
-
 with Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM) as socket:
     socket.bind((HOST, PORT))
     socket.listen()
     socket, _ = socket.accept()
     server(socket)
-
-
-
-
